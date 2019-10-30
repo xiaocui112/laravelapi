51d34104b5ebdc4dd536f1e8cbffce27413d461f
xiaocui112 <cuidaoji@outlook.com>
2019-10-30 22:26:37 +0800

    设置地区

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container con">
         <p class="float-left">
-            由 <a href=" target=" _blank">崔顺</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="" target="_blank">崔顺</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="cuidaoji@outlook.com">联系我们</a></p>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <!-- Branding Image -->
         <a class="navbar-brand " href="{{ url('/') }}">
-            LaraBBS
+            PHP-论坛
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
