25d1a5f413134cfe076c10e9c2b6b4c720c02714
xiaocui112 <cuidaoji@outlook.com>
2019-10-30 22:15:25 +0800

    基础页面结构

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-    <div class="container">
+    <div class="container con">
         <p class="float-left">
             由 <a href=" target=" _blank">崔顺</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
