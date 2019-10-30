5959b8186f41fa6c54f078d60daa87a241035160
xiaocui112 <cuidaoji@outlook.com>
2019-10-30 22:50:23 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
