f139fe813ddded2b136ae442c2b951f9c00c4b1f
xiaocui112 <cuidaoji@outlook.com>
2019-10-31 10:48:18 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,12 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Request $request, Category $category, Topic $topic)
     {
-        $topics = Topic::where('category_id', $category->id)->paginate();
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->with('user', 'category')   // 预加载防止 N+1 问题
+            ->paginate(20);
         return view('topics.index', compact('topics', 'category'));
     }
 }
