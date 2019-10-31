f31900bbc1b54382c42f998c3e91749d8ba11f50
xiaocui112 <cuidaoji@outlook.com>
2019-10-31 12:03:02 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Topic::class);
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'st
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
