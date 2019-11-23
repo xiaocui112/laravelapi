b9dddd421e0757a067adda9701797ec133a22597
xiaocui112 <cuidaoji@outlook.com>
2019-11-23 23:51:50 +0800

    更新包

--- vendor/encore/laravel-admin/resources/views/content.blade.php
+++ vendor/encore/laravel-admin/resources/views/content.blade.php
@@ -21,12 +21,19 @@
                     </li>
                 @else
                 <li>
-                    <a href="{{ admin_url(\Illuminate\Support\Arr::get($item, 'url')) }}">
+                    @if (\Illuminate\Support\Arr::has($item, 'url'))
+                        <a href="{{ admin_url(\Illuminate\Support\Arr::get($item, 'url')) }}">
+                            @if (\Illuminate\Support\Arr::has($item, 'icon'))
+                                <i class="fa fa-{{ $item['icon'] }}"></i>
+                            @endif
+                            {{ $item['text'] }}
+                        </a>
+                    @else
                         @if (\Illuminate\Support\Arr::has($item, 'icon'))
                             <i class="fa fa-{{ $item['icon'] }}"></i>
                         @endif
                         {{ $item['text'] }}
-                    </a>
+                    @endif
                 </li>
                 @endif
             @endforeach
@@ -52,7 +59,11 @@
         @include('admin::partials.exception')
         @include('admin::partials.toastr')
 
-        {!! $content !!}
+        @if($_view_)
+            @include($_view_['view'], $_view_['data']);
+        @else
+            {!! $_content_ !!}
+        @endif
 
     </section>
 @endsection
